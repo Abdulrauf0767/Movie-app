dca06b8220ead35f8e89765ac8c21fce90b8d3aa
Abdulrauf0767 <araof1803@mail.com>
2025-05-17 13:17:31 +0500

    menu icon

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -14,8 +14,12 @@ const Header = () => {
         return () => window.removeEventListener('resize', handleResize);
     }, []);
 
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
     return (
-        <div className="relative overflow-x-hidden">
+        <div className="relative">
             <header className='w-full h-20 flex justify-between items-center bg-[#151b20] z-20 fixed top-0 px-4 md:px-10'>
                 <h1 className='text-red-500 font-roboto text-2xl'>Movie App</h1>
                 <form className='hidden md:flex w-[30%] h-9 rounded-3xl border border-gray-200 items-center relative'>
@@ -35,20 +39,20 @@ const Header = () => {
                         <NavLink to='/signup' className='text-white font-roboto hover:text-red-500 transition'>Signup</NavLink>
                     </ul>
                 </nav>
-                <button className="md:hidden text-white" onClick={() => setIsMenuOpen(!isMenuOpen)}>
+                <button className="md:hidden text-white" onClick={toggleMenu}>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                     </svg>
                 </button>
             </header>
             {isMenuOpen && (
-                <div className="absolute top-20 left-0 w-full bg-[#151b20] md:hidden z-50 py-4 px-6 shadow-lg">
+                <div className="fixed top-20 left-0 w-full bg-[#151b20] md:hidden z-50 py-4 px-6 shadow-lg">
                     <ul className="flex flex-col space-y-4">
-                        <NavLink to='/' onClick={() => setIsMenuOpen(false)} className='text-white font-roboto hover:text-red-500 transition'>Home</NavLink>
-                        <NavLink to='/movies' onClick={() => setIsMenuOpen(false)} className='text-white font-roboto hover:text-red-500 transition'>Movies</NavLink>
-                        <NavLink to='/list' onClick={() => setIsMenuOpen(false)} className='text-white font-roboto hover:text-red-500 transition'>List</NavLink>
-                        <NavLink to='/contact' onClick={() => setIsMenuOpen(false)} className='text-white font-roboto hover:text-red-500 transition'>Contact us</NavLink>
-                        <NavLink to='/signup' onClick={() => setIsMenuOpen(false)} className='text-white font-roboto hover:text-red-500 transition'>Signup</NavLink>
+                        <NavLink to='/' onClick={toggleMenu} className='text-white font-roboto hover:text-red-500 transition'>Home</NavLink>
+                        <NavLink to='/movies' onClick={toggleMenu} className='text-white font-roboto hover:text-red-500 transition'>Movies</NavLink>
+                        <NavLink to='/list' onClick={toggleMenu} className='text-white font-roboto hover:text-red-500 transition'>List</NavLink>
+                        <NavLink to='/contact' onClick={toggleMenu} className='text-white font-roboto hover:text-red-500 transition'>Contact us</NavLink>
+                        <NavLink to='/signup' onClick={toggleMenu} className='text-white font-roboto hover:text-red-500 transition'>Signup</NavLink>
                     </ul>
                     <form className='w-full mt-4 h-9 rounded-3xl border border-gray-200 flex items-center relative'>
                         <input type="search" placeholder='Search here' className='bg-transparent outline-none border-0 w-full h-full pl-5 text-white' />
